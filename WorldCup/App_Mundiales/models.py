a17c972765b83db60c43269edadca9dc750f4afe
Nelson Salazar <urugrafico@gmail.com>
2022-07-30 23:22:49 -0300

    Modelos creados y migrados a la BD

--- WorldCup/App_Mundiales/models.py
+++ WorldCup/App_Mundiales/models.py
@@ -1,3 +1,30 @@
 from django.db import models
 
 # Create your models here.
+
+class Jugador(models.Model):
+
+    nombre = models.CharField(max_length=30)
+    apellido = models.CharField(max_length=30)
+    pais = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)
+
+
+class Estadio(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    capacidad = models.IntegerField
+    ciudad = models.CharField(max_length=50)
+    descripcion = models.CharField(max_length=500)
+
+
+class Cancion(models.Model):
+
+    nombre = models.CharField(max_length=50)
+    interprete = models.CharField(max_length=50)
+    mundial = models.CharField(max_length=50)
+    
+
+
+
+    
