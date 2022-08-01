7f922eed81bd4ce04db05bc208049149ce1ef074
Nelson Salazar <urugrafico@gmail.com>
2022-08-01 00:47:17 -0300

    URL de videos agregada a los modelos

--- WorldCup/App_Mundiales/models.py
+++ WorldCup/App_Mundiales/models.py
@@ -7,15 +7,17 @@ class Jugador(models.Model):
     nombre = models.CharField(max_length=30)
     apellido = models.CharField(max_length=30)
     pais = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=500)
+    descripcion = models.TextField()
+    video_url = models.URLField(blank=True, null=True)
 
 
 class Estadio(models.Model):
 
     nombre = models.CharField(max_length=50)
-    capacidad = models.IntegerField
+    capacidad = models.IntegerField(blank=True, null=True)
     ciudad = models.CharField(max_length=50)
-    descripcion = models.CharField(max_length=500)
+    descripcion = models.TextField()
+    video_url = models.URLField(blank=True, null=True)
 
 
 class Cancion(models.Model):
@@ -23,7 +25,7 @@ class Cancion(models.Model):
     nombre = models.CharField(max_length=50)
     interprete = models.CharField(max_length=50)
     mundial = models.CharField(max_length=50)
-    
+    video_url = models.URLField(blank=True, null=True)
 
 
 
